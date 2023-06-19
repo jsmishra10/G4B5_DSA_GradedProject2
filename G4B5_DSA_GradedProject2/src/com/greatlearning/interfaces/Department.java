f80339e6a32a557a2d43bbf5127c9a09cc86b38f
jsmishra10 <129826592+jsmishra10@users.noreply.github.com>
2023-06-19 16:37:32 +0530

    Add files via upload

---
+++ G4B5_DSA_GradedProject2/src/com/greatlearning/interfaces/Department.java
@@ -0,0 +1,11 @@
+package com.greatlearning.interfaces;
+
+public interface Department {
+
+	public String departmentName();
+
+	public String getTodaysWork();
+
+	public String getWorkDeadline();
+
+}
